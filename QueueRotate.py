f7b922e5080bf0e937123420a05eb53c8f2615b8
osboxes.org <max.test@internet.ru>
2022-07-21 07:47:49 -0400

    5. Queue rotation function. Check is done outside of the loop.

--- QueueRotate.py
+++ QueueRotate.py
@@ -1,8 +1,8 @@
 from Queue import Queue
 
-def rotate_queue(queue, n):
+def rotate_queue(queue, n):    
+    if queue.size() == 0:
+        return
     for i in range(0,n):
         val = queue.dequeue()
-        if val is None:
-            return 
         queue.enqueue(val)
