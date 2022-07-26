4767f960945f3b282414b64c40153cebfc9fc859
osboxes.org <max.test@internet.ru>
2022-07-26 07:09:22 -0400

    8. Hash table.

---
+++ HashTable.py
@@ -0,0 +1,39 @@
+class HashTable:    
+    def __init__(self, sz, stp):
+        self.size = sz
+        self.step = stp
+        self.slots = [None] * self.size
+
+    def hash_fun(self, value):
+        hash = 0
+        for s in value:
+            hash = (hash + ord(s)) % self.size
+        return hash
+
+    def seek_slot(self, value):
+        slot = None        
+        slot = self.hash_fun(value)
+        initialSlot = slot
+        while self.slots[slot] is not None:
+            if self.slots[slot] == value:
+                return slot
+            slot = (slot + self.step) % self.size            
+            if slot == initialSlot:
+                return None      
+        return slot
+
+    def put(self, value):
+        slot = self.seek_slot(value)
+        if slot is not None:
+            self.slots[slot] = value
+        return slot
+
+    def find(self, value):
+        slot = self.hash_fun(value)
+        initialSlot = slot
+        while self.slots[slot] is not None:
+            if self.slots[slot] == value:
+                return slot
+            if slot == initialSlot:
+                return None   
+        return None
---
+++ HashTable_test.py
@@ -0,0 +1,145 @@
+import unittest
+
+from HashTable import HashTable
+
+class TestSeekSlot(unittest.TestCase):
+    
+    def test_seek_slot_empty(self):
+        tab = HashTable(sz = 37, stp = 3)
+        val = 'abc'        
+        slot = tab.seek_slot(val)
+        self.assertEqual(slot, tab.hash_fun(val))
+    
+    def test_seek_slot_collision(self):
+        tab = HashTable(sz = 37, stp = 3)
+        val1 = 'abc'
+        slot1 = tab.seek_slot(val1)
+        tab.slots[slot1] = val1        
+        val2 = '#'
+        slot2 = tab.seek_slot(val2)
+        self.assertEqual(slot2, (tab.hash_fun(val2) + tab.step) % tab.size)
+    
+    def test_seek_slot_full_table(self):
+        size = 4
+        step = 2
+        tab = HashTable(sz = size, stp = step)
+        val = 36
+        values = [chr(val + i*step) for i in range (size//step)]        
+        for v in values:
+            tab.slots[tab.seek_slot(v)] = v        
+        valueToInsert = chr(ord(values[-1]) + step)        
+        self.assertEqual(tab.seek_slot(valueToInsert), None)
+
+    def test_seek_slot_full_table_2(self):
+        size = 5
+        step = 2
+        tab = HashTable(sz = size, stp = step)
+        val = 36
+        values = [chr(val + i*step) for i in range (size)]        
+        for v in values:
+            tab.slots[tab.seek_slot(v)] = v        
+        valueToInsert = chr(ord(values[-1]) + step)        
+        self.assertEqual(tab.seek_slot(valueToInsert), None)
+
+
+class TestPut(unittest.TestCase):
+    
+    def test_put_into_empty_table(self):
+        size = 5
+        step = 2
+        tab = HashTable(sz = size, stp = step)
+        val = chr(36)
+        tab.put(val)
+        self.assertEqual(tab.slots[tab.hash_fun(val)], val)
+        
+    def test_put_one_collision(self):
+        size = 5
+        step = 2
+        tab = HashTable(sz = size, stp = step)
+        val1 = chr(36)
+        slot1 = tab.put(val1)        
+        val2 = chr(36+step)
+        slot2 = tab.put(val2)
+        self.assertEqual(tab.slots[slot1], val1)
+        self.assertEqual(tab.slots[slot2], val2)
+    
+    def test_put_no_free_elements(self):
+        size = 5
+        step = 2
+        tab = HashTable(sz = size, stp = step)
+        val = 36
+        values = [chr(val + i*step) for i in range (size)]        
+        for v in values:
+            tab.put(v)
+        valToPut = chr(ord(values[-1]) + step)
+        slot = tab.put(valToPut)
+        self.assertEqual(slot, None)        
+    
+    def test_put_the_same_value(self):
+        size = 5
+        step = 2
+        tab = HashTable(sz = size, stp = step)
+        val = chr(36)
+        slot1 = tab.put(val)                
+        slot2 = tab.put(val)
+        self.assertEqual(tab.slots[slot1], val)
+        self.assertEqual(slot1, slot2)
+
+
+class TestFind(unittest.TestCase):
+    
+    def test_find_empty(self):
+        size = 5
+        step = 2
+        tab = HashTable(sz = size, stp = step)
+        valToFind = chr(37)
+        valIndex = tab.find(valToFind)
+        self.assertEqual(valIndex, None)
+        
+    def test_find_absent(self):
+        size = 5
+        step = 2
+        tab = HashTable(sz = size, stp = step)
+        val = 36
+        values = [chr(val + i*step) for i in range (size)]        
+        for v in values:
+            tab.put(v)
+        valToFind = chr(ord(values[-1]) + step)
+        slot = tab.find(valToFind)
+        self.assertEqual(slot, None)
+
+    def test_find_no_collision(self):
+        size = 5
+        step = 2
+        tab = HashTable(sz = size, stp = step)
+        val = chr(36)
+        slotExpected = tab.put(val)
+        slotFound = tab.find(val)
+        self.assertEqual(slotExpected, slotFound)
+
+    def test_find_several_collisions(self):
+        size = 5
+        step = 2
+        tab = HashTable(sz = size, stp = step)
+        val = 37
+        values = [chr(val + i*step) for i in range (size)]        
+        slotsActual  = []        
+        slotsExpected  = []        
+        for v in values[:-2:]:
+            slotsActual.append(tab.put(v))
+        for v in values[:-2:]:
+            slotsExpected.append(tab.find(v))         
+        self.assertEqual(slotsExpected, slotsActual)
+        
+    def test_seek_slot_full_table(self):
+        size = 4
+        step = 2
+        tab = HashTable(sz = size, stp = step)
+        val = 36
+        values = [chr(val + i*step) for i in range (size//step)]
+        slotsActual = []
+        for v in values:
+            slotsActual.append(tab.put(v))
+        slotExpected = tab.find(values[-1])     
+        self.assertEqual(slotExpected, slotsActual[-1])
+    
